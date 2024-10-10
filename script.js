3f3d13ca3d4412d4bb408a5747c3657bbb92585b
Christelle Revol <Dreaswolrh@outlook.fr>
2024-10-10 17:24:35 +0200

    modification solution 1

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let nombre1html = document.getElementById("nombre1");
-let nombre1 = nombre1html.value;
+nombre1html.addEventListener("onchange", (event) => {
+	nombre1 = event.target.value;
+});
 
 let nombre2html = document.getElementById("nombre2");
-let nombre2 = nombre2html.value;
+nombre2html.addEventListener("onchange", (event) => {
+	nombre1 = event.target.value;
+});
 
 function launchCalcul() {
 	return nombre1 * nombre2;
 }
-
+/*
 nombre2html.addEventListener("keydown", (e) => {
 	if (e.key === `Enter`) {
 		launchCalcul();
 	}
 });
+*/
 
 console.log(launchCalcul());
-
-let nouveauCalcul = document.getElementById("nouveauCalcul");
-
-nouveauCalcul.addEventListener("click");
